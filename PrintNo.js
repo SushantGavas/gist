d04a1543945cbc5a7764f2e1503327f9c902fbbb
SushantGavas <34272416+SushantGavas@users.noreply.github.com>
2017-12-05 17:44:32 +0530

    PrintNo.js
    Javascript file

---
+++ PrintNo.js
@@ -0,0 +1,21 @@
+/*
+Write a program in JS that prints numbers 1 to 100.
+  For every multiple of 3, print "Earth".
+  For every multiple of 5, print "Jupiter".
+  For every multiple of 9, print "Pluto".
+*/
+ 
+// main logic
+const printNumber = num => {
+  let str = '';
+  num % 5 === 0 ? str = 'Jupiter' :
+    num % 9 === 0 ? str = 'Pluto' :
+      num % 3 === 0 ? str = 'Earth' :
+        str = num;
+  return str;
+};
+ 
+// quick and dirty; could probable generate array and then use that
+for (let i = 1; i <= 100; i++) {
+  console.log(printNumber(i));
+}
